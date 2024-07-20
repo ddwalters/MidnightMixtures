eb967eeacadb9c99988b44d4cc956c8db0be1880
Dylan_W <ddwalters789@gmail.com>
2024-07-20 01:05:08 -0400

    Finalized Cycle function with tweaks

--- Assets/Scripts/PotionsManager.cs
+++ Assets/Scripts/PotionsManager.cs
@@ -15,9 +15,7 @@ public enum PotionType
 public class PotionsManager : MonoBehaviour
 {
     [SerializeField] GameObject potionSlotPrefab;
-
     [SerializeField] GameObject[] slotPositions;
-
     [SerializeField] Sprite waterPotionSprite;
     [SerializeField] Sprite shadowPotionSprite;
     [SerializeField] Sprite flashTextureSprite;
@@ -26,30 +24,35 @@ public class PotionsManager : MonoBehaviour
     Potion selectedPotion;
     List<Potion> potions;
 
-    Potion waterPotion;
-    Potion shadowPotion;
-    Potion flashPotion;
-    Potion explosionPotion;
-
     private void Start()
     {
-        potions = new List<Potion>(4);
-
-        waterPotion = new Potion(waterPotionSprite, PotionType.Water);
-        shadowPotion = new Potion(shadowPotionSprite, PotionType.Shadow);
-        flashPotion = new Potion(flashTextureSprite, PotionType.Flash);
-        explosionPotion = new Potion(explosionTextureSprite, PotionType.Explosion);
-
-        potions.Add(waterPotion);
-        potions.Add(shadowPotion);
-        potions.Add(flashPotion);
-        potions.Add(explosionPotion);
+        potions = new List<Potion>();
     }
 
-    public void AddPotion(Potion potion)
+    public void AddPotion(PotionType potionType)
     {
-        if (!potion.hasCrafted)
+        Potion potion = potions.Find(p => p.potionType == potionType);
+
+        if (potion == null)
         {
+            Sprite potionSprite = null;
+            switch (potionType)
+            {
+                case PotionType.Water:
+                    potionSprite = waterPotionSprite;
+                    break;
+                case PotionType.Shadow:
+                    potionSprite = shadowPotionSprite;
+                    break;
+                case PotionType.Flash:
+                    potionSprite = flashTextureSprite;
+                    break;
+                case PotionType.Explosion:
+                    potionSprite = explosionTextureSprite;
+                    break;
+            }
+
+            potion = new Potion(potionSprite, potionType);
             int latestIndexWithStackCount = -1;
             for (int i = 0; i < potions.Count; i++)
             {
@@ -76,7 +79,7 @@ public class PotionsManager : MonoBehaviour
             }
 
             if (selectedPotion == null)
-                selectedPotion = potions[0];
+                selectedPotion = potion;
         }
         else
             potion.stackCount++;
@@ -95,6 +98,12 @@ public class PotionsManager : MonoBehaviour
 
     public void CyclePotion()
     {
+        if (potions.Count == 0) return;
+
+        Potion firstPotion = potions[0];
+        for (int i = 1; i < potions.Count; i++)
+            potions[i - 1] = potions[i];
+
         int lastIndexWithCount = -1;
         for (int i = 0; i < potions.Count; i++)
         {
@@ -102,20 +111,17 @@ public class PotionsManager : MonoBehaviour
                 lastIndexWithCount = i;
         }
 
-        if (lastIndexWithCount == -1)
-        {
-            Debug.LogError("No potion with stackCount > 0 found.");
-            return;
-        }
-
-        Potion firstPotion = potions[0];
-        for (int i = 1; i < potions.Count; i++)
-            potions[i - 1] = potions[i];
-
         potions[Math.Min(lastIndexWithCount + 1, potions.Count - 1)] = firstPotion;
+
+        for (int i = 0; i < potions.Count; i++)
+        {
+            if (potions[i].slotObject != null)
+                potions[i].slotObject.transform.SetParent(slotPositions[i].transform, false);
+        }
 
         UpdatePotionUI();
     }
+
 
     private void SwapPotionHelper(Potion newSelection)
     {
@@ -138,6 +144,8 @@ public class PotionsManager : MonoBehaviour
 
     public bool UsePotion()
     {
+        if (selectedPotion == null) return false;
+
         selectedPotion.stackCount--;
         UpdatePotionUI();
 
@@ -146,8 +154,10 @@ public class PotionsManager : MonoBehaviour
 
     private void UpdatePotionUI()
     {
-        foreach (Potion potion in potions)
+        for (int i = 0; i < potions.Count; i++)
         {
+            Potion potion = potions[i];
+
             if (!potion.hasCrafted)
                 continue;
 
@@ -176,19 +186,13 @@ public class PotionsManager : MonoBehaviour
         }
     }
 
-    public void TestWaterButton() => AddPotion(waterPotion);
-
-    public void TestShadowButton() => AddPotion(shadowPotion);
-
-    public void TestFlashButton() => AddPotion(flashPotion);
-
-    public void TestExplosionButton() => AddPotion(explosionPotion);
+    public void TestWaterButton() => AddPotion(PotionType.Water);
+    public void TestShadowButton() => AddPotion(PotionType.Shadow);
+    public void TestFlashButton() => AddPotion(PotionType.Flash);
+    public void TestExplosionButton() => AddPotion(PotionType.Explosion);
 
     public void SelectWater() => SelectPotion(PotionType.Water);
-
     public void SelectShadow() => SelectPotion(PotionType.Shadow);
-
     public void SelectFlash() => SelectPotion(PotionType.Flash);
-
     public void SelectExplosion() => SelectPotion(PotionType.Explosion);
 }
