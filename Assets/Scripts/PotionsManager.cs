335c5ab1c011b18a0849c2b589732cd33bbf61c7
Dylan_W <ddwalters789@gmail.com>
2024-07-19 22:31:16 -0400

    Potion Updates

--- Assets/Scripts/PotionsManager.cs
+++ Assets/Scripts/PotionsManager.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 
 public enum PotionType
 {
-    Water,
-    Shadow,
-    Flash,
-    Explosion
+    Water = 1,
+    Shadow = 2,
+    Flash = 3,
+    Explosion = 4
 }
 
 public class PotionsManager : MonoBehaviour
@@ -25,6 +25,8 @@ public class PotionsManager : MonoBehaviour
     [SerializeField] Sprite shadowPotionSprite;
     [SerializeField] Sprite FlashTextureSprite;
     [SerializeField] Sprite ExplosionTextureSprite;
+
+    Potion SelectedPotion;
 
     Potion waterPotion;
     Potion shadowPotion;
@@ -94,7 +96,10 @@ public class PotionsManager : MonoBehaviour
             potion.hasCrafted = true;
             GameObject newPotionSlot = null;
             if (SelectedSlotPosition.transform.childCount == 0)
+            {
+                SelectedPotion = potion;
                 newPotionSlot = Instantiate(newPotion, SelectedSlotPosition.transform);
+            }
             else if (SlotPositionOne.transform.childCount == 0)
                 newPotionSlot = Instantiate(newPotion, SlotPositionOne.transform);
             else if (SlotPositionTwo.transform.childCount == 0)
@@ -148,6 +153,83 @@ public class PotionsManager : MonoBehaviour
         }
     }
 
+    public void SelectPotion(PotionType type)
+    {
+        switch (type)
+        {
+            case PotionType.Water:
+                SwapPotionHelper(waterPotion);
+                break;
+            case PotionType.Shadow:
+                SwapPotionHelper(shadowPotion);
+                break;
+            case PotionType.Flash:
+                SwapPotionHelper(flashPotion);
+                break;
+            case PotionType.Explosion:
+                SwapPotionHelper(explosionPotion);
+                break;
+            default:
+                Debug.Log("Definitly broken homie");
+                return;
+        }
+    }
+
+    private void SwapPotionHelper(Potion newSelection)
+    {
+        if (newSelection == null)
+        {
+            Debug.LogError("New selection potion is null.");
+            return;
+        }
+
+        SpriteRenderer currentChildImage = SelectedPotion.slotObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        if (currentChildImage == null)
+        {
+            Debug.LogError("No Image component found on the child GameObject of the selected potion.");
+            return;
+        }
+
+        TextMeshProUGUI currentChildText = SelectedPotion.slotObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        if (currentChildText == null)
+        {
+            Debug.LogError("No TextMeshPro component found on the child GameObject of the selected potion.");
+            return;
+        }
+
+        SpriteRenderer newChildImage = newSelection.slotObject.transform.GetChild(0).GetComponent<SpriteRenderer>();
+        if (newChildImage == null)
+        {
+            Debug.LogError("No Image component found on the child GameObject of the new selection.");
+            return;
+        }
+
+        TextMeshProUGUI newChildText = newSelection.slotObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        if (newChildText == null)
+        {
+            Debug.LogError("No TextMeshPro component found on the child GameObject of the new selection.");
+            return;
+        }
+
+        // Swap the UI elements
+        Sprite tempSprite = currentChildImage.sprite;
+        string tempText = currentChildText.text;
+
+        currentChildImage.sprite = newChildImage.sprite;
+        currentChildText.text = newChildText.text;
+
+        newChildImage.sprite = tempSprite;
+        newChildText.text = tempText;
+
+        // Swap the slotObject references
+        GameObject tempSlotObject = SelectedPotion.slotObject;
+        SelectedPotion.slotObject = newSelection.slotObject;
+        newSelection.slotObject = tempSlotObject;
+
+        // Update the SelectedPotion reference
+        SelectedPotion = newSelection;
+    }
+
     public bool UsePotion()
     {
         var currentSelectedPotion = waterPotion;
@@ -198,23 +280,19 @@ public class PotionsManager : MonoBehaviour
         return true;
     }
 
-    public void TestWaterButton()
-    {
-        AddPotion(waterPotion);
-    }
+    public void TestWaterButton() => AddPotion(waterPotion);
 
-    public void TestShadowButton()
-    {
-        AddPotion(shadowPotion);
-    }
+    public void TestShadowButton() => AddPotion(shadowPotion);
 
-    public void TestFlashButton()
-    {
-        AddPotion(flashPotion);
-    }
+    public void TestFlashButton() => AddPotion(flashPotion);
 
-    public void TestExplosionButton()
-    {
-        AddPotion(explosionPotion);
-    }
+    public void TestExplosionButton() => AddPotion(explosionPotion);
+
+    public void SelectWater() => SelectPotion(PotionType.Water);
+
+    public void SelectShadow() => SelectPotion(PotionType.Shadow);
+
+    public void SelectFlash () => SelectPotion(PotionType.Flash);
+
+    public void SelectExplosion() => SelectPotion(PotionType.Explosion);
 }
